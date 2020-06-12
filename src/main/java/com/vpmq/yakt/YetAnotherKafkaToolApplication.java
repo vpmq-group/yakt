056f29f413c9edc150d4b64bc47384a6b8f787f7
MaggieQuail <a1sova@mail.ru>
2020-06-12 13:34:32 +0300

    create project structure

---
+++ src/main/java/com/vpmq/yakt/YetAnotherKafkaToolApplication.java
@@ -0,0 +1,13 @@
+package com.vpmq.yakt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherKafkaToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherKafkaToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vpmq/yakt/YetAnotherKafkaToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpmq.yakt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherKafkaToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
